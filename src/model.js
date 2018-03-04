b6d4f7503e84294ebc923d19f6f1b670e20b0532
Andrew Arpasi <andrew@andrewarpasi.com>
2018-03-03 19:50:41 -0500

    get model entity from timelineItem

--- src/model.js
+++ src/model.js
@@ -2,6 +2,22 @@ const dependencies = require('./dependencies')
 const $ = dependencies.jquery
 
 const ModelController = {
+    getModelEntityFromTimelineItem(timelineItem) {
+        const angleX = timelineItem.angleX || 0
+        const angleY = timelineItem.angleY || 0
+        const angleZ = timelineItem.angleZ || 0
+        const rotation = `${angleX} ${angleY} ${angleZ}`
+        const posX = timeline.offsetX || 0
+        const posY = timeline.offsetY || 10
+        const posZ = timeline.offsetZ || 0
+        const position = `${posX} ${posY} ${posZ}`
+        //load assets on event for now, do preloading later
+        const assetID = Assets.addAssetFromURL(timelineItem.resource)
+        const scale = timelineItem.scale || 1
+        this.modelEntity = this.getModelEntity(assetID,scale,rotation,position)
+        return this.modelEntity
+    },
+
     getModelEntity(assetID,scaleFactor,rotation,position) {
         const pos = position || "0 10 0"
         const rot = rotation || "0 0 0"
