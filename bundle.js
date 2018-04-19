6368717503a619152d80a977647c54e29de1b35a
Matin Hormati <mthormati@gmail.com>
2018-04-19 15:14:08 -0400

    Create button to toggle subtitles

--- bundle.js
+++ bundle.js
@@ -36354,6 +36354,8 @@ const Firebase = firebase
 
 const subtitleController = {
     loadSubtitles() {
+        this.subActive = true;
+        this.endSub = false;
         DataController.getLectureAndTimelineFromFirebase((timeline, lecture) => {
             if (lecture['subtitleURL']) {
                 fetch(lecture['subtitleURL'])
@@ -36371,15 +36373,31 @@ const subtitleController = {
                     })
             }
         })
+
+        $('#toggleBtn').on('click', () => {
+            this.subActive = !this.subActive;
+            if (this.subActive) {
+                $('#toggleBtn').text('Sub: ON');
+                if (!this.endSub) {
+                    const subText = this.subs[this.subIndex - 1].text.replace('<br />', '\n');
+                    document.querySelector('#subtitle').setAttribute('value', subText);
+                }
+            } else {
+                $('#toggleBtn').text('Sub: OFF');
+                document.querySelector('#subtitle').setAttribute('value', '');
+            }
+        })
     },
 
     startSubtitles() {
-        // console.log('INITIAL TIME: ', ((new Date()) - this.initialTime) / 1000);
         if (this.subs && this.subIndex < this.subs.length) {
             const endTime = new Date();
             if ((endTime - this.initialTime) / 1000 >= this.subs[this.subIndex].start) {
+                this.endSub = false;
                 const subText = this.subs[this.subIndex].text.replace('<br />', '\n');
-                document.querySelector('#subtitle').setAttribute('value', subText);
+                if (this.subActive) {
+                    document.querySelector('#subtitle').setAttribute('value', subText);
+                }
                 clearTimeout(this.timeoutRef);
                 this.timeoutRef = setTimeout(() => {
                     this.startTime = new Date();
@@ -36388,13 +36406,14 @@ const subtitleController = {
                 this.subIndex++;
             } else {
                 const elapsedTime = ((new Date()) - this.initialTime) / 1000;
+                this.endSub = true;
                 this.timeoutRef = setTimeout(() => {
                     this.startTime = new Date();
                     this.startSubtitles();
                 }, (this.subs[this.subIndex].start - elapsedTime) * 1000);
             }
-
         } else {
+            this.endSub = true;
             document.querySelector('#subtitle').setAttribute('value', '');
         }
     },
@@ -36402,11 +36421,13 @@ const subtitleController = {
     onItemChange(timelineItem) {
         if (timelineItem) {
             if (timelineItem.type === 'video') {
+                this.endSub = false;
                 const pauseEnd = new Date();
                 const pauseTime = pauseEnd - this.pauseStart;
                 this.initialTime -= pauseTime;
                 this.startSubtitles();
             } else {
+                this.endSub = true;
                 this.pauseStart = new Date();
                 if (this.timeoutRef) {
                     clearTimeout(this.timeoutRef);
